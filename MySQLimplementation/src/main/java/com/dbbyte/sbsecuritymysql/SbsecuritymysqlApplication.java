0132298747807c262adf62700f641437a238783b
adriancuria <curia.gheorghe@yahoo.com>
2017-11-25 21:02:45 +0200

    hierachical structure

---
+++ MySQLimplementation/src/main/java/com/dbbyte/sbsecuritymysql/SbsecuritymysqlApplication.java
@@ -0,0 +1,12 @@
+package com.dbbyte.sbsecuritymysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsecuritymysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbsecuritymysqlApplication.class, args);
+	}
+}
--- sbsecuritymysql/README.md
+++
@@ -1,6 +0,0 @@
-### Spring Boot - Security
-
-**Does**
-- authenticate using MySQL database
-- use GrantedAuthority roles for method level security
-- use relations between tables
